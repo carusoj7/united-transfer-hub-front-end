29ff2f0ccc79c70fb4a42319a366da1757616023
Joe Caruso <carusoj7@gmail.com>
2023-06-04 12:44:53 -0500

    set up player service

---
+++ src/pages/NewPlayer/NewPlayer.tsx
@@ -0,0 +1,10 @@
+// npm modules 
+import { useState } from "react";
+
+//services 
+import * as playerService from '../../services/playerService'
+
+// css
+import styles from './NewPlayer.module.css'
+
+const newPlayer = 
---
+++ src/services/playerService.ts
@@ -0,0 +1,5 @@
+// services
+import * as tokenService from './tokenService'
+
+//types 
+import { Player } from '../types/models'
