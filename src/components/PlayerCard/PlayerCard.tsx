32cd91dc7c4aa4ff4c08b9458b2c02474a87b6b8
Joe Caruso <carusoj7@gmail.com>
2023-06-05 17:19:23 -0500

    render player cards

--- src/components/PlayerCard/PlayerCard.tsx
+++ src/components/PlayerCard/PlayerCard.tsx
@@ -15,12 +15,32 @@ const PlayerCard = (props: PlayerCardProps): JSX.Element => {
   const { player, profileName } = props
 
   return (
+    <Box 
+      component= "div"
+      key={player.id}
+      className={styles.playerCard}
+      display="flex" 
+      justifyContent="spread-evenly" 
+      alignItems="center"
+      flexDirection="row"
+      borderRadius="12px"
+      padding="6px"
+      marginTop="6px"
+      >
+    <Box ><img src={player.photo? player.photo:'/BWdog_icon.png'} alt="" className={styles.playercardImg} /></Box>
     <Box className={styles.playerCardContent}>
     <article>
       <h1>{player.name}
         {profileName}
       </h1>
+      <p>Age: {player.age}</p>
+      <p>Position: {player.position}</p>
+      <p>Current Team: {player.team}</p>
+      <p>Estimated Transfer Fee: {player.transferFee}</p>
+      <div>{player.upvotes}</div>
+      <div>{player.upvotes}</div>
     </article>
+    </Box>
     </Box>
   )
 }
