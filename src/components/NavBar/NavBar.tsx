5a7da405d623b8067a4132b45f76a496a9e9ce17
Joe Caruso <carusoj7@gmail.com>
2023-06-05 16:55:59 -0500

    set up navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,6 +4,9 @@ import { NavLink } from 'react-router-dom'
 // types
 import { User } from '../../types/models'
 
+//css 
+import styles from './NavBar.module.css'
+
 interface NavBarProps {
   user: User | null;
   handleLogout: () => void;
@@ -13,11 +16,14 @@ const NavBar = (props: NavBarProps): JSX.Element => {
   const { user, handleLogout } = props
   
   return (
-    <nav>
+    <nav className={styles.nav}>
       {user ?
         <ul>
           <li>Welcome, {user.name}</li>
+          <li><NavLink to="/transferhub">Transfer Hub</NavLink></li>
+          <li><NavLink to="/new"> Add Transfer Target</NavLink></li>
           <li><NavLink to="/profiles">Profiles</NavLink></li>
+          <li><NavLink to="/myprofile">My Profile</NavLink></li>
           <li><NavLink to="" onClick={handleLogout}>LOG OUT</NavLink></li>
           <li><NavLink to="/auth/change-password">Change Password</NavLink></li>
         </ul>
